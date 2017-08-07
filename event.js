04d3aa78c26cb8dcc5463b2fcf3c847ef3eb9daf
James O'Shea <jamesoshea89@hotmail.com>
2017-08-07 13:43:09 +0200

    git artifacts removed

--- event.js
+++ event.js
@@ -13,14 +13,7 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log("hello")
     if (request.greeting == "selection") {
-<<<<<<< HEAD
-      console.log("hello")
-=======
       console.log(request.element)
-      // let classes = request.classString.slice(0, request.classString.length -1).split(' ')
-      // console.log(classes)
->>>>>>> parent of 26af620... image alt tags retrieved
     }
 })
