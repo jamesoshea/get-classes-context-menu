340a7e279afadcbefa9e563c9e4fde1d3f64952f
James O'Shea <jamesoshea89@hotmail.com>
2017-08-14 15:57:16 +0200

    random uids generated

--- popup.js
+++ popup.js
@@ -6,7 +6,11 @@ let state = {
   message: ''
 }
 
-let userId = 'test'
+if(!localStorage.getItem('userId')) {
+  localStorage.setItem('userId', uuidv4())
+}
+
+let userId = localStorage.getItem('userId')
 
 //functions to be run when page loads, esp. click event listeners
 document.addEventListener('DOMContentLoaded', ()=> {
@@ -194,4 +198,11 @@ function setView() {
     makeFieldList()
   }
   document.getElementById('message').innerHTML = state.message
+}
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
